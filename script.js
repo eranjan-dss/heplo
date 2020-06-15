8893b4730afe25bf691d1fd73c06b63a6d63ea76
eranjan <sunethra8023@gmail.com>
2020-06-15 07:32:56 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+var hello = "world"
+console.log(hello)
+if (hello === "world"){
+  let hello = "hello world"
+  console.log(hello)
+  if (hello === "hello world"){
+    let hello = "world"
+    console.log(hello)
+  }
+  console.log(hello.length)
+  
+}
